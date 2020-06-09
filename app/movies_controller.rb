bb75811e4469dd7f3f60b22981ae3ef2e8657b1f
Parker Williamson <parker.williamson@gmail.com>
2020-06-09 19:11:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,7 @@ end
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
-    m.releaste_date = args[:release_date]
+    m.release_date = args[:release_date]
   end
 end
 
